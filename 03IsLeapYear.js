3a0f63ceaa23dc7c55a82eed1951d8a77811a924
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-04-11 12:33:20 -0500

    is leap year

---
+++ 03IsLeapYear.js
@@ -0,0 +1,20 @@
+function isLearYear(year) {
+  if (year % 1 != 0 || year <= 0) {
+    return false;
+  }
+
+  if (year % 4 === 0) {
+    if (year % 100 === 0 && year % 400 === 0) {
+      return true;
+    }
+
+    if (year % 100 === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  return false;
+}
+
+console.log(isLearYear(2020));
