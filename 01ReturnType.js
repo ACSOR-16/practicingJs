c858ab35662e672d3b117118ad050f295f8fbbab
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-04-11 11:46:56 -0500

    firts commit return typeof

---
+++ 01ReturnType.js
@@ -0,0 +1,8 @@
+function solution (value){
+  const returnValue = typeof(value);
+  return returnValue;
+}
+
+console.log(solution(1));
+console.log(solution("oscar"));
+console.log(solution(true));
