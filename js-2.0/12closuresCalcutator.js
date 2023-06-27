a7282e43199770198898686b7a19d3120b7dafa4
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-06-26 21:49:49 -0500

    ejercicio de closures

---
+++ js-2.0/12closuresCalcutator.js
@@ -0,0 +1,47 @@
+function createCalculator() {
+  let total = 0;
+  return {
+    add: function(value){
+      total += value;
+      return total;
+    },
+    subtract: function(value){
+      total -= value;
+      return total;
+    },
+    multiply: function(value){
+      total *= value;
+      return total;
+    },
+    divide: function(value){
+      total /= value;
+      return total;
+    },
+    clear: function(){
+      total =  0;
+      return total;
+    },
+    getTotal: function(){
+      return total;
+    },
+  }
+}
+
+// Input:
+const calculator = createCalculator()
+// calculator.add(10)
+console.log(calculator.add(10));
+// Output: 10
+
+// const calculator = createCalculator()
+// calculator.add(10)
+// calculator.subtract(-10)
+
+// Output: 20
+
+// const calculator = createCalculator()
+// calculator.add(10)
+// calculator.subtract(-10)
+// calculator.clear()
+
+Output: 0
