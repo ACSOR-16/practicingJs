8af6a88b82e77ea3b49a789a277c1e76f648ada0
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-06-28 14:26:00 -0500

    es un cuadrado perfectp

---
+++ js-2.0/16isSquare.js
@@ -0,0 +1,9 @@
+var isSquare = function(n){
+  if (Number.isInteger(Math.sqrt(n))) return "Is square";
+  else console.log("Is not square");
+}
+
+isSquare(3);
+isSquare(4);
+isSquare(9);
+isSquare(26);
