ca64498ef63df3f049a0a3540bf4eba720299946
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-06-30 16:00:36 -0500

    encontrando arrays

--- js-2.0/10ArrayOfArray.js
+++ js-2.0/10ArrayOfArray.js
@@ -1,30 +1,28 @@
-function findArray(arr1, arr2) {
-  let arr3 = arr1.map( element => {
-    if (element === arr2.forEach(element => {
-      return element
-    })) {
-      return element;
-    }
-  });
+function findArray(arr1, arr2){
+  const arr3 = [];
 
-  // for (let i = 0; i < arr1.length; i++) {
-  //   for (let index = 0; index < arr2.length; index++) {
-  //     const element2 = arr2[index];
-  //     console.log(element2 + " pertence al array 2");
-  //     if( i == element2) {
-  //       const element = arr1[i];
-  //       arr3.push(element);
-        
-  //       console.log(element);
-  //     }
-  //   }
-  // }
+  if( arr1.length === 0 || arr2.length === 0){
+    return [];
+  }
+  
+  for (let i = 0; i < arr2.length; i++) {
+      const element = arr1[arr2[i]];
+      arr3.push(element)
+  }
   
   console.log(arr3);
   return arr3;
 }
 
-// findArray(['a', 'a', 'a', 'a', 'a'], [2, 4]);
-// findArray([0, 1, 5, 2, 1, 8, 9, 1, 5], [1, 4, 7]);
-// findArray([1, 2, 3, 4, 5], [0])
+function findArray(arr1, arr2){
+  console.log(
+    arr2.filter( i  => i< arr1.length ).map( i => arr1[i])
+  );
+  // return arr2.filter( i  => i< arr1.length ).map( i => arr1[i] )
+}
+
+findArray(['a', 'a', 'a', 'a', 'a'], [2, 4]);
+findArray([0, 1, 5, 2, 1, 8, 9, 1, 5], [1, 4, 7]);
+findArray([1, 2, 3, 4, 5], [0])
 findArray([1, 2, 3, 4, 5], [4,2,0])
+findArray([1,2,3], [])
