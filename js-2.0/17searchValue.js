c680b2a0f8d781f114631e3532a2ac1adb5b5f31
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-08-28 23:59:21 -0500

    search value

---
+++ js-2.0/17searchValue.js
@@ -0,0 +1,60 @@
+function searchValue(array, value) {
+  const flat = array.flat();
+  if(!flat.some(num => num === value)){
+    throw new Error("value not found")
+  }
+  
+  const local = []
+  let column = null
+
+  array.forEach(element => {
+    // console.log(element)
+    const index = element.findIndex(num => num === value);
+    local.push(index);
+    if(index >= 0 ){
+      column = index
+    } 
+    
+  });
+  console.log(local, column);
+
+  return {
+    row: local.findIndex(num => num >= 0),
+    column: column
+  }
+  
+}
+
+function searchValueTwo(array, value) {
+  const singleArray = array.flatMap((x) => x);
+  const index = singleArray.findIndex((val) => val === value);
+  if (index === -1) {
+    throw new Error("Valor no encontrado");
+  }
+
+  const row = Math.floor(index / array[0].length);
+  const column = index % array[0].length;
+
+  return { row, column };
+}
+
+const row = Math.floor(index / array[0].length);
+const column = index % array[0].length;
+
+return { row, column };
+// Input:
+const array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]
+
+const value = 6
+
+console.log(searchValue(array, value))
+
+// Output:
+// {
+//   row: 1,
+//   column: 1,
+// }
